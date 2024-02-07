02436f95b5894476107203147f6061d3cf29803f
Viateur <akimanaviateur94@gmail.com>
2024-02-07 21:27:36 +0200

    components setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-// import { BrowserRouter, Routes} from "react-router-dom";
-// import { Box } from '@mui/material';
+import React from 'react'
+import Box from '@mui/material/Box';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Feed, VideoDetails, ChannelDetails, SearchItem , Navbar } from "./components";
 
-const App = () => (
-  // <BrowserRouter>
-  //   <Box sx={{ backgroundColor: '#000' }}>
-  //     <Routes>
-  //       <div>App</div>
-  //     </Routes>
-  //   </Box>
-  // </BrowserRouter>
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Box  sx={{backgroundColor:'#000'}}>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Feed/>}/>
+    <Route path="/video/:id" element={<VideoDetails/>}/>
+    <Route path="/channel/:id" element={<ChannelDetails/>}/>
+    <Route path="/search/:searchItem" element={<SearchItem/>}/>
 
-  <div>App</div>
-);
+    </Routes>
+    </Box>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar.jsx";
+export { default as Feed } from "./Feed.jsx";
+export { default as VideoDetails } from "./VideoDetails";
+export { default as ChannelDetails } from "./ChannelDetails.jsx";
+export { default as SearchItem } from "./SearchItem";
